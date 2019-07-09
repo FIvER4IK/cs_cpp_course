691f811cee1ca8f79c91eea6905f2ba674c19a7f
Andrew Shatalov <andrshatalov121@gmail.com>
2019-07-09 15:48:19 +0300

    non test

--- week_1/src/node.cpp
+++ week_1/src/node.cpp
@@ -1,21 +1,21 @@
-/*#include "node.hpp"
-
+#include "node.hpp"
 #include <iostream>
+#include <cstring>
 
-namespace noder
-{
+using namespace std;
 
-namespace
-{
+class node{
 
-/**
- * Находит позицию на которой узел element находится в цепи nod
- * Индексация начинается с 1, 0 - не найден
- */
-/*std::size_t index(node* nod, node* element) {
-    std::size_t size = 1;
-    while (nod)
-    {
+private:
+
+char value;
+node* next;
+
+public:
+
+int size_t index(node* nod, node* element) {
+    size_t size = 1;
+    while (nod){
         if (element == nod)
             return size;
         nod = nod->next;
@@ -26,18 +26,14 @@ namespace
     return 0;
 }
 
-} // namespace
-
-
-node* create_node (char letter) {
+int create_node (char letter) {
     node* result = new node;
     result->value = letter;
     result->next = nullptr;
-    
     return result;
 }
 
-std::size_t get_length (node* nod) {
+size_t get_length (node* nod) {
     return index(nod, nullptr);
 }
 
@@ -47,20 +43,20 @@ bool is_circular (node* nod) {
 
 void print (node* nod) {
     while (nod) {
-        std::cout << nod->value;
+        cout << nod->value;
         nod = nod->next;
     }
 }
 
-node* concate (node* left, node* right) {
+node concate (node* left, node* right) {
     node* result = left;
-    return result;
-    // Написать
-}
+        return result;
+        // Написать
+    }
 
 void insert (node* nod, std::size_t i, node* chain) {
-    // Написать
-}
+    node* i = chain;
 
-} // noder
-*/
+    }
+
+};
